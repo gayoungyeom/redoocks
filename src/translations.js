f5ff36f5f8ff052689fbbb277542b80ef1ab25c2
gayoungyeom <gayoungyeom@gmail.com>
2021-01-04 14:04:23 +0900

    [4-5] Hypertranslate(useContext)

---
+++ src/translations.js
@@ -0,0 +1,12 @@
+const translations = {
+  es: {
+    'Hello!': 'Hola!',
+    Translate: 'Traducir',
+  },
+  en: {
+    'Hola!': 'Hello!',
+    Traducir: 'Translate',
+  },
+};
+
+export default translations;
