6062be02014bc160d1ae747f2801abc9ebf2c105
gayoungyeom <gayoungyeom@gmail.com>
2021-01-04 17:15:59 +0900

    [6-9] ToDos(useReducer)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,74 @@
-import React from 'react';
-import Screen from './Screen';
-import Lang from './context';
-import translations from './translations';
+import { useState, useReducer } from 'react';
+import { v4 as uuid } from 'uuid';
+
+const initialState = {
+  toDos: [],
+};
+
+const ADD = 'add';
+const DEL = 'del';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD:
+      return {
+        toDos: [
+          ...state.toDos,
+          {
+            text: action.payload,
+            id: uuid(),
+          },
+        ],
+      };
+    case DEL:
+      return {
+        toDos: state.toDos.filter((toDO) => toDO.id !== action.payload),
+      };
+    default:
+      return;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newToDo, setNewToDo] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ADD, payload: newToDo });
+    setNewToDo('');
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewToDo(value);
+  };
+
   return (
-    <Lang defaultLang='en' translations={translations}>
-      <Screen />
-    </Lang>
+    <>
+      <h1>Add to do</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={newToDo}
+          type='text'
+          placeholder='Write to do'
+          onChange={onChange}
+        />
+      </form>
+
+      <ul>
+        <h2>To Dos</h2>
+        {state.toDos.map((toDo) => (
+          <li key={toDo.id}>
+            <span>{toDo.text}</span>
+            <button onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+              ❌
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
